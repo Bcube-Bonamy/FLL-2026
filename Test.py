6948ab3a833d61b7b291470dcf88979150fd62b7
Bcube-Bonamy <bcubebonamy@gmail.com>
2025-11-29 11:00:08 +0800

    Luh time to debug after fair

--- Test.py
+++ Test.py
@@ -1,1 +1,8 @@
-print("hello world")
+from llm_axe.models import OllamaChat
+from llm_axe import ObjectDetectorAgent
+
+llm = OllamaChat(model="llava:7b")
+detector = ObjectDetectorAgent(llm, llm)
+
+resp = detector.detect(images=["Water-Barrels.jpg"], detection_criteria="Things related to water")
+print(resp)
