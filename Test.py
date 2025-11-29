100d63f1839590bbcf2ad125e11612f0ebea7806
Bcube-Bonamy <bcubebonamy@gmail.com>
2025-11-29 16:51:22 +0800

    Ill look into API tas creds kc mabilis daw cus ts

--- Test.py
+++ Test.py
@@ -1,8 +1,8 @@
-from llm_axe.models import OllamaChat
+from llm_axe import OllamaChat
 from llm_axe import ObjectDetectorAgent
 
-llm = OllamaChat(model="llava:7b")
-detector = ObjectDetectorAgent(llm, llm)
+llm = OllamaChat(model="llava:latest")
+detector = ObjectDetectorAgent(llm, llm, 0.3, 0.3, False)
 
-resp = detector.detect(images=["Water-Barrels.jpg"], detection_criteria="Things related to water")
+resp = detector.detect(images=["./Water-Barrels.jpg"], detection_criteria="Things related to water and the wall behind them")
 print(resp)
